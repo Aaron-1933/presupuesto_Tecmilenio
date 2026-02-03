0afd75b3bcb8394d733631b47199f62e9c74ca21
AaronxpiDev <aaronmcp66@gmail.com>
2026-02-02 21:03:15 -0600

    funciones pruebas

--- js/app.js
+++ js/app.js
@@ -1,22 +1,55 @@
-const cargarCabecero = () => {
-    var presupuesto = totalIngresos() - totalEgresos();
-    var porcentajeEgreso = totalEgresos() / totalIngresos();
+let egresos = {
+    Renta: 900,
+    Ropa: 400
+};
+
+let ingresos = {
+    Quincena: 9000,
+    Venta: 400
+};
+
+
+const cargarCabecero =  () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalEgresos() / totalIngresos();
+
+    console.log("Presupuesto: ", formatoMoneda(presupuesto));
+    console.log("Porcentaje: ", formatoPorcentaje(porcentajeEgreso));
+    console.log("Ingresos: ", formatoMoneda(totalIngresos()));
+    console.log("Egresos: ", formatoMoneda(totalEgresos()));
 }
 
-const totalIngresos = ()=> {
+const totalIngresos = () => {
     let totalIngresos = 0;
-    for (let ingreso of ingresos) {
-        totalIngresos += ingreso.valor;
+    for(let ingreso of Object.values(ingresos)) { //lo tuve que cambair a objeto para probar
+        totalIngresos += ingreso;
     }
     return totalIngresos;
 }
 
 const totalEgresos = () => {
     let totalEgreso = 0;
-
-    for (let egreso of egresos) {
-        totalEgreso += egreso.valor;
+    for(let egreso of Object.values(egresos)) {
+        totalEgreso += egreso; //x2
     }
 
     return totalEgreso;
 }
+
+const formatoMoneda = (valor) => { 
+    return valor.toLocaleString('es-Mx', {
+        style: 'currency',
+        currency: 'MXN',
+        minimunFractionDigits: 2
+    });
+}
+
+const formatoPorcentaje = (valor) => {
+    return valor.toLocaleString('es-MX', {
+        style: 'percent',
+        minimumFractionDigits: 2
+    });
+}
+
+
+cargarCabecero();
